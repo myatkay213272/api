2ade808b94da3f30c2d4528a82080636cfea63be
z672004682004 <z672004682004@gmail.com>
2024-05-15 12:40:13 +0630

    add database mutation queries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config()
+
+const db = require('./db')
+const DB_HOST = process.env.DB_HOST
+
+db.connect(DB_HOST)
+
+
+const models = require('./models')
+
 const port = process.env.PORT || 4000
 
 const {ApolloServer, gql } = require('apollo-server-express')
@@ -22,10 +32,17 @@ const typeDefs = gql`
 
 const resolvers = {
     Query:{
-        
+        notes: async () => {
+            return await models.Note.find();
+        }
     },
     Mutation:{
-
+        newNote: async(parent, args)=>{
+            return await models.Note.create({
+                content: args.content,
+                author: args.author
+            })
+        }
     }
 }
 
@@ -40,7 +57,7 @@ apolloServerStart()
 
 
 app.listen({port}, ()=>{
-    console.log(`Graphql playground: https://localhost:${port}/api`)
-    console.log(`Graphql Server: https://localhost:${port}${server.graphqlPath}`)
+    console.log(`Graphql playground: http://localhost:${port}/api`)
+    console.log(`Graphql Server: http://localhost:${port}${server.graphqlPath}`)
 })
 
